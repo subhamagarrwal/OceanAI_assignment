5d8a7ecea88640f6f87fe018d7d20bda10bb5efe
subhamagarrwal <130570909+subhamagarrwal@users.noreply.github.com>
2025-11-19 15:12:54 +0530

    Initial commit - clean code without secrets

---
+++ sample.py
@@ -0,0 +1,24 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    # This is the default and can be omitted
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
